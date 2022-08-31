bec268ff1f833772437f8f9718f69c4f4533b069
Hrvoje <hbrkljacic@gmail.com>
2022-08-31 18:46:30 -0400

    Solids

---
+++ src/com/cydeo/X.java
@@ -0,0 +1,15 @@
+package com.cydeo.solid;
+
+public class X {
+
+    public static void main(String[] args) {
+
+        Z z = new Y();
+
+        z.method();
+
+        //loosely coupled structure
+
+
+    }
+}
---
+++ src/com/cydeo/Y.java
@@ -0,0 +1,11 @@
+package com.cydeo.solid;
+
+public class Y implements Z{
+
+    @Override
+    public void method() {
+
+        //Gets broke for example
+
+    }
+}
